6cb4cec325518e8bbe2af8a4d6c9534cc218fe96
Prashant-Yadav <prashantyadav.indore@gmail.com>
2016-01-05 16:01:21 +0530

    Student and mentor registration implemented.

--- project_handler/projects_app/forms.py
+++ project_handler/projects_app/forms.py
@@ -34,12 +34,24 @@ class StudentRegistrationForm(forms.Form):
 
 	password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)))
 
+	def clean(self):
+		if ('password1' in self.cleaned_data) and ('password2' in self.cleaned_data):
+			if (self.cleaned_data['password1']) != (self.cleaned_data['password2']):
+				raise forms.ValidationError("The two password fields did not match.")
+		return self.cleaned_data
+
 
 class MentorRegistrationForm(forms.Form):
 	mentor_name = forms.CharField(label="Full Name", max_length="50")
 
 	email = forms.EmailField()
 
-	password1 = forms.CharField(label="Password", max_length="30")
+	password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)))
 
-	password2 = forms.CharField(label="Confirm Password", max_length="30")
+	password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)))
+
+	def clean(self):
+		if ('password1' in self.cleaned_data) and ('password2' in self.cleaned_data):
+			if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+				raise forms.ValidationError("The two password fields did not match.")
+		return self.cleaned_data
--- project_handler/projects_app/views.py
+++ project_handler/projects_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
 
-from .models import Course, Batch, User, Student
+from .models import Course, Batch, User, Student, Mentor
 from .forms import StudentRegistrationForm, MentorRegistrationForm
 
 def index(request):
@@ -15,29 +15,28 @@ def registration(request):
 		
 		if student_form.is_valid():
 			name = student_form.cleaned_data['student_name']
-			student_roll_no = student_form.cleaned_data['roll_no']
-			course = student_form.cleaned_data['course']
-			sem = student_form.cleaned_data['sem']
-			batch = student_form.cleaned_data['batch']
-			email_id = student_form.cleaned_data['email']
-			password = student_form.cleaned_data['password1']
 			
 			try:
-				user = User(user_name=name, password=password)
+				user = User(user_name=name, password=student_form.cleaned_data['password1'])
 				user.save()
-				course = Course.objects.get(course_name=course)
-				batch = Batch.objects.get(batch_name=batch)
-				student = Student(student_name=name, roll_no=student_roll_no, semester=sem, email=email_id, course=course.id, batch=batch.id, user=user.id)
+				student = Student(student_name=name, roll_no=student_form.cleaned_data['roll_no'], semester=int(student_form.cleaned_data['sem']), email=student_form.cleaned_data['email'], course=student_form.cleaned_data['course'], batch=student_form.cleaned_data['batch'], user=user)
 				student.save()
-				return HttpResponse("Submission successful.")
+				return HttpResponse("Student registered")
 			except:
-				raise Http404("name=%s, roll_no=%s, course=%s, batch=%s, sem=%s, user=%s, user_id=%s, email=%s, password=%s" % (name, student_roll_no, course.id, batch.id, sem, user, user.id, email_id, password))
+				raise Http404("Student registration failed.")
 			
 		elif mentor_form.is_valid():
 			name = mentor_form.cleaned_data['mentor_name']
-			email = mentor_form.cleaned_data['email']
-			password = mentor_form.cleaned_data['password1']
-			return HttpResponse('Mentor form')
+			
+			try:
+				user = User(user_name=name, password=mentor_form.cleaned_data['password1'])
+				user.save()
+				mentor = Mentor(mentor_name=name, email=mentor_form.cleaned_data['email'], user=user)
+				mentor.save()
+				return HttpResponse("Mentor Registered.")
+			except:
+				raise Http404("Mentor registration failed.")
+
 		else:
 			return HttpResponse('forms are invalid.')
 
